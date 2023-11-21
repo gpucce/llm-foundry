9889bdf0befb022adbb3663c5a756da01227adaf
gpucce <g.puccetti92@gmail.com>
2023-11-21 12:06:28 +0100

    more generation scripts

--- scripts/inference/vllm_generate.py
+++ scripts/inference/vllm_generate.py
@@ -111,6 +111,8 @@ def parse_args() -> Namespace:
     parser.add_argument("--system_prompt", type=str, default=None)
     parser.add_argument("--human_key", type=str, default=None)
     parser.add_argument("--tensor_parallel_size", type=int, default=1)
+    parser.add_argument("--dataset_name", type=str, default="")
+    parser.add_argument("--use_beam_search", type=str, default="true")
     return parser.parse_args()
 
 
@@ -128,18 +130,30 @@ def load_prompt_string_from_file(prompt_path_str: str):
         prompt_string = [json.loads(i) if i.startswith("{") else i.replace("\n", "") for i in prompt_string]
     return prompt_string
 
+def str2bool(x):
+    if x == "true":
+        return True
+    elif x == "false":
+        return False
+
 def main(args):
     tensor_parallel_size = args.tensor_parallel_size
     print(f"########################### Tensor Parallel {tensor_parallel_size}")
+    use_beam_search = str2bool(args.use_beam_search)
+    print(use_beam_search)
     prompt_dicts = load_prompt_string_from_file(
         args.prompts
     )
 
-    prompt_dicts = prompt_dicts[:args.max_prompts]
-    prompts = [i["prompt"] for i in prompt_dicts]
     if args.system_prompt is None:
         args.system_prompt = "[INST]<<SYS>>\n \n<</SYS>>\n\n {} [/INST]"
-    prompts = [args.system_prompt.format(i) for i in prompts]
+        
+    prompt_dicts = prompt_dicts[:args.max_prompts]
+    for idx in range(len(prompt_dicts)):
+        prompt_dicts[idx]["prompt"] = args.system_prompt.format(
+            prompt_dicts[idx]["prompt"]
+        )
+    prompts = [i["prompt"] for i in prompt_dicts]
 
     llm = LLM(model=args.name_or_path, tensor_parallel_size=tensor_parallel_size)
 
@@ -159,10 +173,13 @@ def main(args):
         future_df_path = future_df_path / _future_df_path
 
         params = SamplingParams(
-            temperature=temp,
-            top_p=topp,
-            top_k=topk,
+            temperature=temp if not use_beam_search else 0,
+            top_p=topp if not use_beam_search else 1,
+            top_k=topk if not use_beam_search else -1,
             max_tokens=args.max_new_tokens,
+            repetition_penalty=1.2,
+            use_beam_search=use_beam_search,
+            best_of=3 if use_beam_search else 1
             # ignore_eos=True
         )
 
@@ -172,18 +189,24 @@ def main(args):
             args.human_key = "human_text"
         with open(str(future_df_path).replace(".csv", ".json"), "w") as jf:
             for prompt_dict, generated in tqdm(zip(prompt_dicts, generations)):
+                new_prompt_dict = {}
                 future_df.append({
                     "prompt": prompt_dict["prompt"],
                     "original_continuation": prompt_dict[args.human_key],
                     "continuation": generated.outputs[0].text
                 })
-                prompt_dict["machine_text"] = generated.outputs[0].text
-                prompt_dict["model"] = (
+                new_prompt_dict["prompt"] = prompt_dict["prompt"]
+                new_prompt_dict["human_text"] = prompt_dict[args.human_key]
+                new_prompt_dict["machine_text"] = generated.outputs[0].text
+                new_prompt_dict["model"] = (
                     args.name_or_path.split("/")[-1]
                     if len(args.name_or_path.split("/")[-1]) > 0
                     else args.name_or_path.split("/")[-2]
                 )
-                jf.write(json.dumps(prompt_dict) + "\n")
+                new_prompt_dict["source"] = args.dataset_name
+                new_prompt_dict["source_ID"] = ""
+                jf.write(json.dumps(new_prompt_dict))
+                jf.write("\n")
             pd.DataFrame(future_df).to_csv(future_df_path)
 
 if __name__ == "__main__":
