171e4ec59fd40d9c06d9361ee7e5d4bd8b8a3895
gpucce <g.puccetti92@gmail.com>
2023-11-30 18:39:54 +0100

    more scripts

--- scripts/eval/sbatch_scripts/run_current_eval.sh
+++ scripts/eval/sbatch_scripts/run_current_eval.sh
@@ -3,11 +3,11 @@
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:4
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=00:30:00
+#SBATCH --time=02:00:00
 #SBATCH --cpus-per-task=32
 #SBATCH --account=IscrC_GELATINO
 #SBATCH --partition=boost_usr_prod
-#SBATCH --qos=boost_qos_dbg
+#SBATCH --qos=normal
 #SBATCH --output=./slurm_logs/slurm-%j.out
 
 module load gcc
